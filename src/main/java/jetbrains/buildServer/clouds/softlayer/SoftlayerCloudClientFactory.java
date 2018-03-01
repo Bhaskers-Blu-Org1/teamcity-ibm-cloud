0cc74f5aa22f9085742c12f2346f11b3845b2b22
Scott Wyman Neagle <scottwn@ibm.com>
2018-03-01 17:10:17 -0600

    wrote SoftlayerCloudClientFactory.java

---
+++ src/main/java/jetbrains/buildServer/clouds/softlayer/SoftlayerCloudClientFactory.java
@@ -0,0 +1,58 @@
+package jetbrains.buildServer.clouds.azure.arm
+
+import jetbrains.buildServer.clouds.*
+import jetbrains.buildServer.clouds.base.AbstractCloudClientFactory
+import jetbrains.buildServer.clouds.base.errors.TypedCloudErrorInfo
+import jetbrains.buildServer.serverSide.AgentDescription
+import jetbrains.buildServer.serverSide.PropertiesProcessor
+import jetbrains.buildServer.serverSide.ServerSettings
+import jetbrains.buildServer.web.openapi.PluginDescriptor
+import java.util.*
+
+class SoftlayerCloudClientFactory 
+    extends AbstractCloudClientFactory<SoftlayerCloudImageDetails,
+                                       SoftlayerCloudClient>
+{
+    @NotNull
+    @Override
+    public SoftlayerCloudClient createNewClient(@NotNull final CloudState state,
+            @NotNull final Collection<SoftlayerCloudImageDetails> images,
+            @NotNull final CloudClientParameters params)
+    {
+        return new SoftlayerCloudClient();
+    }
+
+    @NotNull
+    @Override
+    public SoftlayerCloudClient createNewClient(@NotNull final CloudState state,
+            @NotNull final CloudClientParameters params,
+            final TypedCloudErrorInfo[] profileErrors);
+    {
+        return new SoftlayerCloudClient();
+    }
+
+    @NotNull
+    @Override
+    public Collection<SoftlayerCloudImageDetails> parseImageData(
+            CloudClientParameters params)
+    {
+        Collection<SoftlayerCloudImageDetails> output
+            = new ArrayList<SoftlayerCloudImageDetails>;
+        output.add(new SoftlayerCloudImageDetails());
+        return output;
+    }
+
+    @NotNull
+    public String getDisplayName()
+    {
+        return "SoftLayer";
+    }
+
+    @Nullable
+    @Override
+    protected TypedCloudErrorInfo[] checkClientParams(
+            @NotNull final CloudClientParameters params)
+    {
+        return new TypedCloudErrorInfo[0];
+    }
+}
