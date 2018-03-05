e47e3030a6e209d443fec55866dfbfb673cafa58
Scott Wyman Neagle <scottwn@ibm.com>
2018-03-05 12:32:37 -0600

    added SoftlayerCheckedCloudException.java

--- src/main/java/jetbrains/buildServer/clouds/softlayer/SoftlayerCheckedCloudException.java
+++ src/main/java/jetbrains/buildServer/clouds/softlayer/SoftlayerCheckedCloudException.java
@@ -1,1 +1,22 @@
 package jetbrains.buildServer.clouds.softlayer;
+
+import jetbrains.buildServer.clouds.base.errors.CheckedCloudException;
+
+public class SoftlayerCheckedCloudException extends CheckedCloudException
+{
+    public SoftlayerCheckedCloudException(final Throwable cause)
+    {
+        super(cause.getMessage(), cause);
+    }
+
+    public SoftlayerCheckedCloudException(final String message)
+    {
+        super(message);
+    }
+
+    public SoftlayerCheckedCloudException(final String message,
+            final Throwable cause)
+    {
+        super(message, cause);
+    }
+}
