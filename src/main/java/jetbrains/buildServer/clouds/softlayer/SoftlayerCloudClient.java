29bc6b51234d21a614d4c8f5c962bf571eebe509
Scott Wyman Neagle <scottwn@ibm.com>
2018-03-07 14:51:43 -0600

    modify SoftlayerCloudClient so that it implements CloudClientEx

--- src/main/java/jetbrains/buildServer/clouds/softlayer/SoftlayerCloudClient.java
+++ src/main/java/jetbrains/buildServer/clouds/softlayer/SoftlayerCloudClient.java
@@ -1,14 +1,61 @@
 package jetbrains.buildServer.clouds.softlayer;
 
+import jetbrains.buildServer.clouds.*;
 import jetbrains.buildServer.clouds.base.AbstractCloudClient;
 import jetbrains.buildServer.clouds.base.tasks.UpdateInstancesTask;
 import jetbrains.buildServer.serverSide.AgentDescription;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class SoftlayerCloudClient extends AbstractCloudClient<
-        SoftlayerCloudInstance, SoftlayerCloudImage, SoftlayerCloudImageDetails>
+public class SoftlayerCloudClient implements CloudClientEx
 {
+    public SoftlayerCloudClient(CloudClientParameters params)
+    {
+        System.out.println("new client");
+    }
+
+    public boolean isInitialized()
+    {
+        return false;
+    }
+
+    public SoftlayerCloudImage findImageById(String imageId)
+        throws CloudException
+    {
+        return null;
+    }
+
+    public Collection<? extends CloudImage> getImages() throws CloudException
+    {
+        return Collections.emptyList();
+    }
+
+    public CloudErrorInfo getErrorInfo()
+    {
+        return new CloudErrorInfo("test");
+    }
+
+    public boolean canStartNewInstance(CloudImage image)
+    {
+        return false;
+    }
+
+    public String generateAgentName(AgentDescription agentDescription)
+    {
+        return "agent name";
+    }
+
+    public String generateAgentName(SoftlayerCloudImage image, String instanceId)
+    {
+        return "name for " + instanceId;
+    }
+
+    public CloudInstance startNewInstance(CloudImage image,
+        CloudInstanceUserData data)
+    {
+        return ((LocalCloudImage)image).startNewInstance(data);
+    }
+
     @Nullable
     public SoftlayerCloudInstance findInstanceByAgent(@NotNull final 
             AgentDescription agentDescription)
@@ -17,18 +64,36 @@ public class SoftlayerCloudClient extends AbstractCloudClient<
         return new SoftlayerCloudInstance(checkAndCreateImage(imageDetails));
     }
 
-    @Override
-    protected SoftlayerCloudImage checkAndCreateImage(
-            @NotNull final SoftlayerCloudImageDetails imageDetails)
+    public void restartInstance(@NotNull final CloudInstance instance)
     {
-        return new SoftlayerCloudImage();
+        ((SoftlayerCloudInstance) instance).restart();
     }
 
-    @NotNull
-    @Override
-    protected UpdateInstancesTask<SoftlayerCloudInstance, SoftlayerCloudImage,
-              SoftlayerCloudClient> createUpdateInstancesTask()
+    public void terminateInstance(@NotNull final CloudInstance instance)
     {
-        return new UpdateInstancesTask(new SoftlayerApiConnector(), this); 
+        ((LocalCloudInstance) instance).terminate();
+    }
+
+    public void dispose()
+    {
+        System.out.println("dispose");
+    }
+
+    @Nullable
+    private SoftlayerCloudImage findImage(
+            @NotNull final AgentDescription agentDescription)
+    {
+        final String imageId
+            = agentDescription.getConfigurationParameters().get(
+                    LocalCloudConstants.IMAGE_ID_PARAM_NAME);
+        return imageId == null ? null : findImageById(imageId);
+    }
+
+    @Nullable
+    private String findInstanceId(
+            @NotNull final AgentDescription agentDescription)
+    {
+        return agentDescription.getConfigurationParameters().get(
+                LocalCloudConstants.INSTANCE_ID_PARAM_NAME);
     }
 }
