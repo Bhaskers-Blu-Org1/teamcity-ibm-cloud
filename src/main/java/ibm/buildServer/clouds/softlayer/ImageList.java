47fc4cf07bd2e7e0f097816dc8eaffeea0b8b99f
Vidhi <Vidhi.Shah@ibm.com>
2018-03-15 12:28:24 -0500

    Dependency solved

--- src/main/java/ibm/buildServer/clouds/softlayer/ImageList.java
+++ src/main/java/ibm/buildServer/clouds/softlayer/ImageList.java
@@ -8,41 +8,37 @@
 package ibm.buildServer.clouds.softlayer;
 
 //import com.squareup.okhttp.OkHttpClient;
+import java.io.IOException;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import java.io.IOException;
+
+import org.springframework.http.client.OkHttpClientHttpRequestFactory;
 
 public class ImageList {
 	
-	OkHttpClient client = new OkHttpClient();
-
-	  String run(String url) throws IOException {
-	    Request request = new Request.Builder()
-	        .url(url)
-	        .build();
-
-	    try (Response response = client.newCall(request).execute()) {
-	      return response.body().string();
-	    }
-	  }
-
+	
 	
 	public static void main(String[] args) {
 		System.out.println("hell");
-		
-		ImageList example = new ImageList();
-	    String response;
-		try {
-			response = example.run("https://raw.github.com/square/okhttp/master/README.md");
-			 System.out.println(response);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	   
+		try
+		{
+		OkHttpClient client = new OkHttpClient();
 
-		
+		Request request = new Request.Builder()
+		  .url("https://vidhi.shah@ibm.com:c0e5a3602aa6eb56bce8a575aa975d2cf2b2c40893308bfb736831b1c741beda@api.softlayer.com/rest/v3/SoftLayer_Account/PrivateBlockDeviceTemplateGroups.json")
+		  .get()
+		  .addHeader("Cache-Control", "no-cache")
+		  .addHeader("Postman-Token", "ca1428a6-8b8c-4abd-9c09-bdb7f176bf95")
+		  .build();
+
+		Response response = client.newCall(request).execute();
+		System.out.println(response);
+		}
+		catch(IOException e)
+		{
+			
+		}
 		
 	}
 
