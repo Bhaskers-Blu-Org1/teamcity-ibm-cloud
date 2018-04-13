aff9144db94d5cd5bccc492b4f7095bdd4d3e8ca
Scott Wyman Neagle <scottwn@ibm.com>
2018-04-13 15:51:49 -0500

    implemented necessary changes in SoftlayerCloudClientFactory

--- src/main/java/ibm/buildServer/clouds/softlayer/SoftlayerCloudClient.java
+++ src/main/java/ibm/buildServer/clouds/softlayer/SoftlayerCloudClient.java
@@ -1,81 +1,98 @@
 package ibm.buildServer.clouds.softlayer;
 
 import jetbrains.buildServer.clouds.*;
+import jetbrains.buildServer.clouds.base.connector.CloudAsyncTaskExecutor;
 import jetbrains.buildServer.serverSide.AgentDescription;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import java.util.Collection;
 import java.util.Collections;
 
-public class SoftlayerCloudClient implements CloudClientEx
-{
-    public SoftlayerCloudClient(CloudClientParameters params)
-    {
-        System.out.println("new client");
-    }
+public class SoftlayerCloudClient implements CloudClientEx {
+  boolean initialized = false;
+  private final CloudAsyncTaskExecutor executor;
+  int taskDelayTime = 60 * 1000; // Time in milliseconds.
+  private final Map<String, SoftlayerCloudImage> images;
+
+  public SoftlayerCloudClient(CloudClientParameters params) {
+    executor = new CloudAsyncTaskExecutor(
+        "Async tasks for cloud " + params.getProfileDescription());
+    images = new HashMap<String, SoftlayerCloudImage>();
+  }
     
-    public boolean isInitialized()
-    {
-        return false;
-    }
+  public boolean isInitialized() {
+    return initialized;
+  }
 
-    public SoftlayerCloudImage findImageById(String imageId)
-        throws CloudException
-    {
-        return null;
-    }
+  public SoftlayerCloudImage findImageById(String imageId)
+      throws CloudException {
+    return null;
+  }
 
-    public Collection<? extends CloudImage> getImages() throws CloudException
-    {
-        return Collections.emptyList();
-    }
+  public Collection<? extends CloudImage> getImages() throws CloudException {
+    return Collections.emptyList();
+  }
 
-    public CloudErrorInfo getErrorInfo()
-    {
-        return null;
-    }
+  public CloudErrorInfo getErrorInfo() {
+    return null;
+  }
 
-    public boolean canStartNewInstance(CloudImage image)
-    {
-        return false;
-    }
+  public boolean canStartNewInstance(CloudImage image) {
+    return image.canStartNewInstance();
+  }
 
-    public String generateAgentName(AgentDescription agentDescription)
-    {
-        return "agent name";
-    }
+  public String generateAgentName(AgentDescription agentDescription) {
+    return "agent name";
+  }
 
-    public String generateAgentName(SoftlayerCloudImage image, String instanceId)
-    {
-        return "name for " + instanceId;
-    }
+  public String generateAgentName(SoftlayerCloudImage image, String instanceId) {
+    return "name for " + instanceId;
+  }
 
-    public CloudInstance startNewInstance(CloudImage image,
-            CloudInstanceUserData data)
-    {
-    		return ((SoftlayerCloudImage)image).startNewInstance(data);
-    }
-    
-    @Nullable
-    public SoftlayerCloudInstance findInstanceByAgent(@NotNull final 
-            AgentDescription agentDescription)
-    {
-        return null;
-    }
-    
-    public void restartInstance(@NotNull final CloudInstance instance)
-    {
-        
-    }
+  public CloudInstance startNewInstance(CloudImage image,
+      CloudInstanceUserData data) {
+    return ((SoftlayerCloudImage) image).startNewInstance(data);
+  }
 
-    public void terminateInstance(@NotNull final CloudInstance instance)
-    {
-        
+  @Nullable
+  public SoftlayerCloudInstance findInstanceByAgent(
+      @NotNull final AgentDescription agentDescription) {
+    final String instanceName = agent.getConfigurationParameters()
+      .get("softlayer.instance.name");
+    if(instanceName == null) {
+      return null;
     }
-    
-    public void dispose()
-    {
-        
+    for(SoftlayerCloudImage image : images.values()) {
+      final SoftlayerCloudInstance instance
+        = image.findInstanceById(instanceName);
+      if(instance != null) {
+        return instance;
+      }
     }
+    return null
+  }
 
+  public void terminateInstance(@NotNull final CloudInstance instance) {
+    instance.getImage().terminateInstance(instance); 
+  }
+
+  public void start() {
+    SoftlayerUpdateInstancesTask updateInstancesTask
+      = new SoftlayerUpdateInstancesTask(this);
+    executor.submit("Client start", new Runnable() {
+      public void run() {
+        try {
+          updateInstancesTask.run();
+          executor.scheduleWithFixedDelay(
+              "Update instances",
+              updateInstancesTask,
+              taskDelayTime,
+              taskDelayTime,
+              TimeUnit.MILLISECONDS);
+        } finally {
+          initialized = true;
+        }
+      }
+    });
+  }
 }
--- src/main/java/ibm/buildServer/clouds/softlayer/SoftlayerCloudClientFactory.java
+++ src/main/java/ibm/buildServer/clouds/softlayer/SoftlayerCloudClientFactory.java
@@ -15,88 +15,83 @@ import org.jetbrains.annotations.Nullable;
 //imports from java api
 import java.util.*;
 
-class SoftlayerCloudClientFactory implements CloudClientFactory
-{
+class SoftlayerCloudClientFactory implements CloudClientFactory {
+  private PluginDescriptor pluginDescriptor;
+  private final String settingPagePath;
+  @NotNull private final CloudManagerBase myCloudManager;
 
-	private PluginDescriptor pluginDescriptor;
-	private final String settingPagePath;
-	@NotNull private final CloudManagerBase myCloudManager;
-	
-	public SoftlayerCloudClientFactory(@NotNull CloudRegistrar cloudRegistrar, @NotNull final CloudManagerBase cloudManager, @NotNull final PluginDescriptor pluginDescriptor){
-		
-		//Registering plugin with cloud.
-		cloudRegistrar.registerCloudFactory(this);
-		//plugin's settings page
-		settingPagePath = pluginDescriptor.getPluginResourcesPath(SoftlayerCloudConstants.SETTINGS_HTML_PAGE);
-		myCloudManager = cloudManager;
-	}
-	
-	
-	@NotNull
-	  public CloudClientEx createNewClient(@NotNull CloudState state, @NotNull CloudClientParameters params)
-	  {
-		return new SoftlayerCloudClient(params);
-	  }
-	
-	 @NotNull
-	  public String getCloudCode()
-	  {
-		 return SoftlayerCloudConstants.CLOUD_CODE;
-	  }
+  public SoftlayerCloudClientFactory(
+      @NotNull CloudRegistrar cloudRegistrar,
+      @NotNull final CloudManagerBase cloudManager,
+      @NotNull final PluginDescriptor pluginDescriptor) {
+    // Registering plugin with cloud.
+    cloudRegistrar.registerCloudFactory(this);
+    // Set path to plugin's settings page.
+    settingPagePath = pluginDescriptor.getPluginResourcesPath(
+        SoftlayerCloudConstants.SETTINGS_HTML_PAGE);
+    myCloudManager = cloudManager;
+  }
 
-	  /**
-	   * Description to be shown on the web pages
-	   * @return display name to be shown to the user
-	   */
-	  @NotNull
-	  public String getDisplayName()
-	  {
-		  return SoftlayerCloudConstants.DISPLAY_NAME;
-	  }
+  @NotNull
+  public CloudClientEx createNewClient(
+      @NotNull CloudState state, @NotNull CloudClientParameters params) {
+    SoftLayerCloudClient client = new SoftlayerCloudClient(params);
+    client.start();
+    return client;
+  }
 
+  @NotNull
+  public String getCloudCode() {
+    return SoftlayerCloudConstants.CLOUD_CODE;
+  }
 
-	  /**
-	   * Properties editor jsp
-	   * @return properties editor JSP file
-	   * @since 5.1
-	   */
-	  @Nullable
-	  public String getEditProfileUrl()
-	  {
-		  return settingPagePath;
-	  }
+  /**
+  * Description to be shown on the web pages
+  * @return display name to be shown to the user
+  */
+  @NotNull
+  public String getDisplayName() {
+	return SoftlayerCloudConstants.DISPLAY_NAME;
+  }
 
-	  /**
-	   * Return initial values for form parameters.
-	   * @return map of initial values
-	   */
-	  @NotNull
-	  public Map<String,String> getInitialParameterValues()
-	  {
-		  return Collections.emptyMap();
-	  }
+  /**
+  * Properties editor jsp
+  * @return properties editor JSP file
+  * @since 5.1
+  */
+  @Nullable
+  public String getEditProfileUrl() {
+	return settingPagePath;
+  }
 
-	  /**
-	   * Returns the properties processor instance (validator).
-	   *
-	   * @return properties processor
-	   */
-	  @NotNull
-	  public PropertiesProcessor getPropertiesProcessor()
-	  {
-		  return new SoftlayerCloudPropertiesProcessor(myCloudManager);
-	  }
+  /**
+  * Return initial values for form parameters.
+  * @return map of initial values
+  */
+  @NotNull
+  public Map<String,String> getInitialParameterValues() {
+	return Collections.emptyMap();
+  }
 
-	  /**
-	   * Checks it the agent could be an instance of one of the running profiles.
-	   * This method is called to check weather it is needed to open connection
-	   * for a cloud profiles of that type to check if the agent is started
-	   * from a cloud profiles of that type.
-	   * @param description agent info to check
-	   * @return true if this agent could be an instance of that cloud type
-	   */
-	  public boolean canBeAgentOfType(@NotNull AgentDescription description)
-	  {
-		  return true;
-	  }
+  /**
+  * Returns the properties processor instance (validator).
+  *
+  * @return properties processor
+  */
+  @NotNull
+  public PropertiesProcessor getPropertiesProcessor() {
+    return new SoftlayerCloudPropertiesProcessor(myCloudManager);
+  }
+
+  /**
+  * Checks it the agent could be an instance of one of the running profiles.
+  * This method is called to check weather it is needed to open connection
+  * for a cloud profiles of that type to check if the agent is started
+  * from a cloud profiles of that type.
+  * @param description agent info to check
+  * @return true if this agent could be an instance of that cloud type
+  */
+  public boolean canBeAgentOfType(@NotNull AgentDescription description) {
+	return true;
+  }
 }
