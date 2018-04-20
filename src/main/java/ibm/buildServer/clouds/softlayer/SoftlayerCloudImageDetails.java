5618305b1574227c476fb39aa7a43ca24e30f84e
Scott Wyman Neagle <scottwn@ibm.com>
2018-04-20 11:42:51 -0500

    merged SoftlayerCloudImageDetails.java from Vidhi

--- src/main/java/ibm/buildServer/clouds/softlayer/SoftlayerCloudImageDetails.java
+++ src/main/java/ibm/buildServer/clouds/softlayer/SoftlayerCloudImageDetails.java
@@ -14,23 +14,34 @@ public class SoftlayerCloudImageDetails{
 	
 
 	public String datacenter;
-	public boolean localDiskFlag;
+	public String localDiskFlag;
 	public String vsiTemplate;
 	public long maxCores;
 	public long maxMemory;
-	
+	public String domainName;
+	public int agentPoolId;
 	
 	
 	public SoftlayerCloudImageDetails(@NotNull final CloudImageParameters imageParameters){
+		agentPoolId = imageParameters.getAgentPoolId();
 		agentName = imageParameters.getParameter(SoftlayerCloudConstants.AGENT_NAME);
 		datacenter = imageParameters.getParameter(SoftlayerCloudConstants.DATACENTER_LIST);
-		localDiskFlag = Boolean.parseBoolean(imageParameters.getParameter(SoftlayerCloudConstants.DISK_TYPE));
-		vsiTemplate = imageParameters.getParameter(SoftlayerCloudConstants.VSI_TEMPLATE_LIST);
+		localDiskFlag = imageParameters.getParameter(SoftlayerCloudConstants.DISK_TYPE);
+		domainName = imageParameters.getParameter(SoftlayerCloudConstants.DOMAIN_NAME);
 		maxCores = Long.parseLong(imageParameters.getParameter(SoftlayerCloudConstants.MAX_CORES));
 		maxMemory = Long.parseLong(imageParameters.getParameter(SoftlayerCloudConstants.MAX_MEMORY));
 		network = imageParameters.getParameter(SoftlayerCloudConstants.NETWORK);
+		vsiTemplate = imageParameters.getParameter(SoftlayerCloudConstants.VSI_TEMPLATE_LIST);
 		profileId = imageParameters.getParameter(SoftlayerCloudConstants.PROFILE_ID);
 		sourceId = imageParameters.getParameter(SoftlayerCloudConstants.SOURCE_ID);
+	}
+
+	public int getAgentPoolId() {
+		return agentPoolId;
+	}
+
+	public void setAgentPoolId(int agentPoolId) {
+		this.agentPoolId = agentPoolId;
 	}
 	
 	String getSourceId() {
@@ -73,11 +84,11 @@ public class SoftlayerCloudImageDetails{
 		this.datacenter = datacenter;
 	}
 
-	public boolean isLocalDiskFlag() {
+	public String getLocalDiskFlag() {
 		return localDiskFlag;
 	}
 
-	public void setLocalDiskFlag(boolean localDiskFlag) {
+	public void setLocalDiskFlag(String localDiskFlag) {
 		this.localDiskFlag = localDiskFlag;
 	}
 
@@ -105,6 +116,14 @@ public class SoftlayerCloudImageDetails{
 		this.maxMemory = maxMemory;
 	}
 	
+	public String getDomainName() {
+		return domainName;
+	}
+
+	public void setDomainName(String domainName) {
+		this.domainName = domainName;
+	}
+	
 	//int getMaxInstances();
 	
 	 @Override
@@ -112,10 +131,14 @@ public class SoftlayerCloudImageDetails{
 		  String str = "Image Details: {\nProfile Id:"+ profileId+
 				  		",\nNetwork:"+network+",\nSource Id:"+sourceId+
 				  		",\nAgentName:"+agentName+",\nDatacenter:"+datacenter+
-				  		",\nIsLocalDisk:"+localDiskFlag+",\nVSI Template:"+vsiTemplate+
+				  		",\nDiskType:"+localDiskFlag+",\nVSI Template:"+vsiTemplate+
 				  		",\nMax Cores:"+maxCores+",\nMax Memory:"+maxMemory+
+				  		",\nDomain:"+domainName+
+				  		",\nAgent Pool Id:"+Integer.toString(agentPoolId)+
 				        "\n}";
 		  return str;
 	  }
+	 
+	 
 	
 }
