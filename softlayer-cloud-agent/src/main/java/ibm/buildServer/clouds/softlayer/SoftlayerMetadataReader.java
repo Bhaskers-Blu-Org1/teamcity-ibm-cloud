b6e722dc597c553301372d1e132ec4e4e1544dd6
Scott Wyman Neagle <scottwn@ibm.com>
2018-05-11 15:54:58 -0500

    added updateConfiguration

--- softlayer-cloud-agent/src/main/java/ibm/buildServer/clouds/softlayer/SoftlayerMetadataReader.java
+++ softlayer-cloud-agent/src/main/java/ibm/buildServer/clouds/softlayer/SoftlayerMetadataReader.java
@@ -5,28 +5,60 @@
 
 package ibm.buildServer.clouds.softlayer;
 
+import com.intellij.openapi.diagnostic.Logger;
+import jetbrains.buildServer.log.Loggers;
 import jetbrains.buildServer.util.EventDispatcher;
 import jetbrains.buildServer.agent.AgentLifeCycleListener;
 import jetbrains.buildServer.agent.AgentLifeCycleAdapter;
 import jetbrains.buildServer.agent.BuildAgent;
+import jetbrains.buildServer.agent.BuildAgentConfigurationEx;
+import jetbrains.buildServer.clouds.CloudInstanceUserData;
 
 import com.softlayer.api.*;
 import com.softlayer.api.service.resource.Metadata;
 
 public class SoftlayerMetadataReader {
-  public SoftlayerMetadataReader(EventDispatcher<AgentLifeCycleListener> events) {
+  private Logger LOG = Loggers.AGENT;
+
+  public SoftlayerMetadataReader(EventDispatcher<AgentLifeCycleListener> events,
+      BuildAgentConfigurationEx configuration) {
+    LOG.info("SoftLayer plugin initializing... via MetadataReader");
     events.addListener(new AgentLifeCycleAdapter() {
       @Override
       public void afterAgentConfigurationLoaded(BuildAgent agent) {
+        LOG.info("Softlayer loading configuration");
         fetchConfiguration();
       }
     })
   }
 
   private void fetchConfiguration() {
-    ApiClient client = new RestApiClient();
-    Metadata.Service metadataService = Metadata.service(client);
-    String metadata = metadataService.getUserMetadata();
-    updateConfiguration(metadata);
+    try {
+      ApiClient client = new RestApiClient();
+      Metadata.Service metadataService = Metadata.service(client);
+      String metadata = metadataService.getUserMetadata();
+      updateConfiguration(metadata);
+    } catch (Exception e) {
+      LOG.warn("MetadataReader error: " + e);
+    }
   }
 
+  private void updateConfiguration(String json) {
+    try {
+      CloudInstanceUserData data = CloudInstanceUserData.deserialize(json);
+      if(data == null) {
+        LOG.info("SoftLayer integration is not available; no TeamCity data");
+        LOG.debug(json);
+        return;
+      }
+      LOG.info(
+          "SoftLayer integration is available; will register " 
+          + data.getAgentName()
+          + " on server URL "
+          + data.getServerAddress());
+      configuration.serverUrl = data.getServerAddress();
+    } catch (Exception e) {
+      LOG.warn("SoftlayerMetadataReader error: " + e);
+    }
+  }
+}
