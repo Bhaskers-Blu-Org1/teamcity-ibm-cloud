e3861ed789a9ffaf7173ca11c7f4578e1e59a825
Vidhi <Vidhi.Shah@ibm.com>
2018-06-15 12:30:38 -0500

    Issue107: Added detailed comments on IBMCloudClientFactory.java file

--- ibm-cloud-server/src/main/java/ibm/buildServer/clouds/ibm/IBMCloudClientFactory.java
+++ ibm-cloud-server/src/main/java/ibm/buildServer/clouds/ibm/IBMCloudClientFactory.java
@@ -1,6 +1,11 @@
 /*
-* @author: Scott Wyman Neagle 
-* scottwn@ibm.com
+* @author: Scott Wyman Neagle, scottwn@ibm.com.
+* 
+* Purpose of file:
+* - TC server will invoke 'IBMCloudClientFactory' which has CloudClientFacoty.
+* - Everytime TC server will restart, new 'IBMCloudClientFactory' object will be created.
+* - 'IBMCloudClientFactory' provides details on cloud_code, display_name, 
+* registering plugin as cloud plugin and setting plugin's setting's page url.
 **/
 
 package ibm.buildServer.clouds.ibm;
@@ -13,70 +18,89 @@ import jetbrains.buildServer.clouds.*;
 import jetbrains.buildServer.clouds.server.CloudManagerBase;
 import jetbrains.buildServer.serverSide.*;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import jetbrains.buildServer.log.Loggers;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import jetbrains.buildServer.log.Loggers;
+
 import com.intellij.openapi.diagnostic.Logger;
 
 public class IBMCloudClientFactory implements CloudClientFactory {
+	
+  /* 1) pluginDescriptor: USed for setting plugin resource paths like: settings page path. 
+   * 2) LOG: TC server log. "<TC root path>/logs/teamcity-server.log" 
+   * 3) clients: HashMap to keep track of the created cloud profiles, HashMap Key = ProjectId + ProfileId.
+   */
   private PluginDescriptor pluginDescriptor;
   private final String settingPagePath;
-  @NotNull private final CloudManagerBase myCloudManager;
+  @NotNull
+  private final CloudManagerBase myCloudManager;
   private final static Logger LOG = Loggers.SERVER;
-  //use hashmap to keep track of the created clients
-  private final Map<String, IBMCloudClient> clients =
-		    new ConcurrentHashMap<>();
+  private final Map<String, IBMCloudClient> clients = new ConcurrentHashMap<>();
 
-  public IBMCloudClientFactory(
-      @NotNull CloudRegistrar cloudRegistrar,
-      @NotNull final CloudManagerBase cloudManager,
-      @NotNull final PluginDescriptor pluginDescriptor) {
-    // Registering plugin with cloud.
+  /* Constructor. 
+   * 1) cloudRegistrar: Used to register plugin in cloud using,'IBMCloudClientFactory' object. 
+   * 2) settingPagePath: Set path to plugin's settings page using pluginDescriptor.
+   */
+  public IBMCloudClientFactory (@NotNull CloudRegistrar cloudRegistrar,
+      @NotNull final CloudManagerBase cloudManager, @NotNull final PluginDescriptor pluginDescriptor) {
+
     cloudRegistrar.registerCloudFactory(this);
-    // Set path to plugin's settings page.
     settingPagePath = pluginDescriptor.getPluginResourcesPath(IBMCloudConstants.SETTINGS_HTML_PAGE);
     myCloudManager = cloudManager;
   }
 
+  /* 1) createNewClient(): called when you create/edit new cloud profile. 
+   * And, when cloud profile is created/edited, this function will be called for all existing cloud profiles too. 
+   * 2) [line: 69] Check HashMap to match existing cloud profiles and add new cloud profiles to HashMap.
+   * 3) [line: 83] Adding all images to their cloud profile object 'client'. 
+   * 
+   * @return just created new client instance.
+   */
   @NotNull
-  public IBMCloudClient createNewClient(
-      @NotNull CloudState state, @NotNull CloudClientParameters params) {
-	  String clientId = state.getProjectId() + state.getProfileId();
-	  IBMCloudClient client;
-	  boolean createdNewClient = false;
-      boolean clientHasImage = false;
-	  if (clients.containsKey(clientId)) {
-		  client = clients.get(clientId);
-	  } else {
-		  client = new IBMCloudClient(params);
-		  clients.put(clientId, client);
-		  createdNewClient = true;
-	  }
-    for(IBMCloudImageDetails imageDetails : parseImageData(params)) {
-      // Print to the screen during test; logging has not been implemented in automated
-      // unit tests.
-      System.out.println("creating image for " + params.getParameter(IBMCloudConstants.USER_NAME));
+  public IBMCloudClient createNewClient(@NotNull CloudState state, 
+      @NotNull CloudClientParameters params) {
+    
+    String clientId = state.getProjectId() + state.getProfileId();
+    IBMCloudClient client;
+    boolean createdNewClient = false;
+    boolean clientHasImage = false;
+    
+    if (clients.containsKey(clientId)) client = clients.get(clientId); 
+    else {
+      client = new IBMCloudClient(params);
+      clients.put(clientId, client);
+      createdNewClient = true;
+    }
+    
+    for (IBMCloudImageDetails imageDetails : parseImageData(params)) {
+      
+      // Unit Test: Print to the screen during test.
+      System.out.println("creating image for " + params.getParameter(IBMCloudConstants.USER_NAME)); 
       IBMCloudImage image = new IBMCloudImage(imageDetails);
-      image.setCredentials(params.getParameter(IBMCloudConstants.USER_NAME), 
-    		  params.getParameter(IBMCloudConstants.API_KEY));
+      image.setCredentials(params.getParameter(IBMCloudConstants.USER_NAME),
+          params.getParameter(IBMCloudConstants.API_KEY));
       client.addImage(image);
       clientHasImage = true;
     }
-    if(clientHasImage) {
-      LOG.info("Checking for running instances on each image for " + clientId);
+    
+    if (clientHasImage) {
       client.retrieveRunningInstances();
     }
-    if(createdNewClient) {
+    
+    if (createdNewClient) {
       client.start();
-    } else {
-      //The updateInstancesTask stops working when user updates images, so needs to restart.
+    } else {                                                            
+      // Restart updateInstancesTask when user updates images.
       client.restartUpdateInstancesTask(params);
     }
     return client;
   }
 
+  /* Gets list of all cloud images defined in corresponding cloud profile.
+   * */
   public Collection<IBMCloudImageDetails> parseImageData(@NotNull final CloudClientParameters params) {
-	return params.getCloudImages().stream().map(IBMCloudImageDetails::new).collect(Collectors.toList());
+    return params.getCloudImages().stream().map(IBMCloudImageDetails::new).collect(Collectors.toList());
   }
 
   @NotNull
@@ -85,52 +109,51 @@ public class IBMCloudClientFactory implements CloudClientFactory {
   }
 
   /**
-  * Description to be shown on the web pages
-  * @return display name to be shown to the user
-  */
+   * Description to be shown on the web pages
+   * @return display name to be shown to the user
+   */
   @NotNull
   public String getDisplayName() {
-	return IBMCloudConstants.DISPLAY_NAME;
+    return IBMCloudConstants.DISPLAY_NAME;
   }
 
   /**
-  * Properties editor jsp
-  * @return properties editor JSP file
-  * @since 5.1
-  */
+   * Properties editor jsp
+   * @return properties editor JSP file
+   * @since 5.1
+   */
   @Nullable
   public String getEditProfileUrl() {
-	return settingPagePath;
+    return settingPagePath;
   }
 
   /**
-  * Return initial values for form parameters.
-  * @return map of initial values
-  */
+   * Return initial values for form parameters.
+   * @return map of initial values
+   */
   @NotNull
-  public Map<String,String> getInitialParameterValues() {
-	return Collections.emptyMap();
+  public Map<String, String> getInitialParameterValues() {
+    return Collections.emptyMap();
   }
 
   /**
-  * Returns the properties processor instance (validator).
-  *
-  * @return properties processor
-  */
+   * Returns the properties processor instance (validator).
+   * @return properties processor
+   */
   @NotNull
   public PropertiesProcessor getPropertiesProcessor() {
     return new IBMCloudPropertiesProcessor(myCloudManager);
   }
 
   /**
-  * Checks it the agent could be an instance of one of the running profiles.
-  * This method is called to check weather it is needed to open connection
-  * for a cloud profiles of that type to check if the agent is started
-  * from a cloud profiles of that type.
-  * @param description agent info to check
-  * @return true if this agent could be an instance of that cloud type
-  */
+   * Checks it the agent could be an instance of one of the running profiles. This
+   * method is called to check weather it is needed to open connection for a cloud
+   * profiles of that type to check if the agent is started from a cloud profiles
+   * of that type.
+   * @param description agent info to check
+   * @return true if this agent could be an instance of that cloud type
+   */
   public boolean canBeAgentOfType(@NotNull AgentDescription description) {
-	return description.getConfigurationParameters().containsKey("INSTANCE_NAME");
+    return description.getConfigurationParameters().containsKey("INSTANCE_NAME");
   }
 }
