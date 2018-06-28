48f8d5761e3a27c2665fea76f3f326487db686ad
Scott Wyman Neagle <scottwn@ibm.com>
2018-06-28 16:38:40 -0500

    #107 Fix formatting in IBMCloudPropertiesProcessor

--- ibm-cloud-server/src/main/java/ibm/buildServer/clouds/ibm/IBMCloudPropertiesProcessor.java
+++ ibm-cloud-server/src/main/java/ibm/buildServer/clouds/ibm/IBMCloudPropertiesProcessor.java
@@ -12,25 +12,26 @@ import java.util.*;
 public class IBMCloudPropertiesProcessor implements PropertiesProcessor {
   @NotNull 
   private final CloudManagerBase myCloudManager;
-	
-  public IBMCloudPropertiesProcessor(@NotNull final CloudManagerBase cloudManager){
+
+  public IBMCloudPropertiesProcessor(
+      @NotNull final CloudManagerBase cloudManager){
     myCloudManager = cloudManager;
   }
 
   @NotNull
-  public Collection<InvalidProperty> process(final Map<String, String> properties) {
-		
+  public Collection<InvalidProperty> process(
+      final Map<String, String> properties) {
     List<InvalidProperty> list = new ArrayList<InvalidProperty>();
     notEmpty(properties,IBMCloudConstants.USER_NAME, list);
     notEmpty(properties,IBMCloudConstants.SECURE_API_KEY, list);
-	    
     return list;
   }
-	
-  private void notEmpty(@NotNull final Map<String, String> props, @NotNull final String key, 
-		  @NotNull final Collection<InvalidProperty> col) {
-		
-    // null check exception
+
+  /**
+   * Null check.
+   */
+  private void notEmpty(@NotNull final Map<String, String> props,
+      @NotNull final String key, @NotNull final Collection<InvalidProperty> col) {
     if (!props.containsKey(key) || StringUtil.isEmptyOrSpaces(props.get(key))) {
       col.add(new InvalidProperty(key, "Value should be set"));
     }
