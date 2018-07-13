88042872cddcb9be0347e52dfbc907230729f363
Vidhi <Vidhi.Shah@ibm.com>
2018-07-13 10:09:38 -0500

    issue195 adding comment

--- ibm-cloud-server/src/main/java/ibm/buildServer/clouds/ibm/IBMCloudInstance.java
+++ ibm-cloud-server/src/main/java/ibm/buildServer/clouds/ibm/IBMCloudInstance.java
@@ -214,17 +214,16 @@ public class IBMCloudInstance implements CloudInstance {
 
   /**
    * called in #start(). Write instance Id into a file.
+   * file @ /root/TeamCity/bin
+   * instanceInfo has information of profileId, imageId and vsiId: "IBMSL-10 0 51234567"
+   * instanceInfo and clientAndImage (from IBMCloudClient.java) should match
    */
   private void writeInstanceId() {
     try {
-      // file @ /root/TeamCity/bin
       File file = new File(image.TEAMCITY_INSTANCES);
       file.createNewFile();
       FileWriter fw = new FileWriter(file, true);
       PrintWriter writer = new PrintWriter(fw);
-      /* instanceInfo has information of profileId, imageId and vsiId: "IBMSL-10 0 51234567"
-       * instanceInfo and clientAndImage (from IBMCloudClient.java) should match
-       */
       String instanceInfo = userData.getProfileId() + " " + getImageId() + " " + id;
       writer.write(instanceInfo);
       writer.close();
@@ -269,13 +268,13 @@ public class IBMCloudInstance implements CloudInstance {
   /**
    * called by IBMUpdateInstancesTask when instance is RUNNING.
    * Serialize CloudInstanceUserData, and set as SoftLayer user metadata.
-   * Add two configuration parameters: instance name and image name.
+   * Add two configuration parameters: instance name and image id.
+   * Configuration parameter: 'ibm.image.id' needs to be same as images hashmap key in IBMCloudClient.java
    */
   public void setMetadata() {
     try {
       List<String> userDataList = new ArrayList<String>();
       userData.addAgentConfigurationParameter("ibm.instance.name", name);
-      //ibm.image.name needs to be same as images hashmap key in IBMCloudClient.java
       userData.addAgentConfigurationParameter("ibm.image.id", getImageId());
       userDataList.add(userData.serialize());
       Long virtualGuestId = new Long(getInstanceId());
